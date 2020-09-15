e3c6156d99673bd6c7d8f84296d0049c8563cd33
t-matsumo <tatsukimatsumo@gmail.com>
2020-09-16 01:39:37 +0900

    convert MathLib

---
+++ src/main/kotlin/jp/atcoder/library/kotlin/math/MathLib.kt
@@ -0,0 +1,101 @@
+package jp.atcoder.library.kotlin.math
+
+internal object MathLib {
+    private fun safe_mod(x: Long, m: Long): Long {
+        var x = x
+        x %= m
+        if (x < 0) x += m
+        return x
+    }
+
+    private fun inv_gcd(a: Long, b: Long): LongArray {
+        var a = a
+        a = safe_mod(a, b)
+        if (a == 0L) return longArrayOf(b, 0)
+        var s = b
+        var t = a
+        var m0: Long = 0
+        var m1: Long = 1
+        while (t > 0) {
+            val u = s / t
+            s -= t * u
+            m0 -= m1 * u
+            var tmp = s
+            s = t
+            t = tmp
+            tmp = m0
+            m0 = m1
+            m1 = tmp
+        }
+        if (m0 < 0) m0 += b / s
+        return longArrayOf(s, m0)
+    }
+
+    fun pow_mod(x: Long, n: Long, m: Long): Long {
+        var x = x
+        var n = n
+        assert(n >= 0 && m >= 1)
+        var ans: Long = 1
+        while (n > 0) {
+            if (n % 2 == 1L) ans = ans * x % m
+            x = x * x % m
+            n /= 2
+        }
+        return ans
+    }
+
+    fun crt(r: LongArray, m: LongArray): LongArray {
+        assert(r.size == m.size)
+        val n = r.size
+        var r0: Long = 0
+        var m0: Long = 1
+        for (i in 0 until n) {
+            assert(1 <= m[i])
+            var r1 = safe_mod(r[i], m[i])
+            var m1 = m[i]
+            if (m0 < m1) {
+                var tmp = r0
+                r0 = r1
+                r1 = tmp
+                tmp = m0
+                m0 = m1
+                m1 = tmp
+            }
+            if (m0 % m1 == 0L) {
+                if (r0 % m1 != r1) return longArrayOf(0, 0)
+                continue
+            }
+            val ig = inv_gcd(m0, m1)
+            val g = ig[0]
+            val im = ig[1]
+            val u1 = m1 / g
+            if ((r1 - r0) % g != 0L) return longArrayOf(0, 0)
+            val x = (r1 - r0) / g % u1 * im % u1
+            r0 += x * m0
+            m0 *= u1
+            if (r0 < 0) r0 += m0
+            //System.err.printf("%d %d\n", r0, m0);
+        }
+        return longArrayOf(r0, m0)
+    }
+
+    fun floor_sum(n: Long, m: Long, a: Long, b: Long): Long {
+        var a = a
+        var b = b
+        var ans: Long = 0
+        if (a >= m) {
+            ans += (n - 1) * n * (a / m) / 2
+            a %= m
+        }
+        if (b >= m) {
+            ans += n * (b / m)
+            b %= m
+        }
+        val y_max = (a * n + b) / m
+        val x_max = y_max * m - b
+        if (y_max == 0L) return ans
+        ans += (n - (x_max + a - 1) / a) * y_max
+        ans += floor_sum(y_max, a, m, (a - x_max % a) % a)
+        return ans
+    }
+}
