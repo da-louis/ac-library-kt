5bb2d36195a9d529bee4fc921c91a6159fc27323
t-matsumo <tatsukimatsumo@gmail.com>
2020-10-18 13:04:22 +0900

    Add probrem URL

--- src/test/kotlin/jp/atcoder/library/kotlin/dsu/DSUTest.kt
+++ src/test/kotlin/jp/atcoder/library/kotlin/dsu/DSUTest.kt
@@ -1,3 +1,5 @@
+// verification-helper: PROBLEM https://judge.yosupo.jp/problem/unionfind
+
 package jp.atcoder.library.kotlin.dsu
 
 //import org.junit.Assert.*
--- src/test/kotlin/jp/atcoder/library/kotlin/dsu/DSU_test.java
+++
@@ -1,8 +0,0 @@
-// verification-helper: PROBLEM https://judge.yosupo.jp/problem/unionfind
-package jp.atcoder.library.kotlin.dsu;
-
-public class DSU_test {
-    public static void main(String[] args) {
-        new DSUTest().verifyWithLibraryChecker();
-    }
-}
